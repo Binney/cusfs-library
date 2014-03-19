b8884d3bb43b4aeb239bbd3f7897ea6e97c49819
SBinney <sar.binney@hotmail.co.uk>
2014-03-19 18:25:58 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CusfsLibrary::Application.config.secret_key_base = '19db2e29b4f15190e24130d6240459d4d7f4dafedffd635b864436c5cab3e9f877fe10cfafa734bed6699805db1d69a7c0758776a24cfb691e4e24463065a2b4'
